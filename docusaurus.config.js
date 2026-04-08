68dc97ba25cc7556c6ad5705f2a37287e2bb541b
proanimer <1322767102@qq.com>
2026-04-08 12:44:30 +0800

    chore: 更新生产环境URL为更简洁的地址

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://java2-top-git-master-drowningincodes-projects.vercel.app',
+  url: 'https://java2-top.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
